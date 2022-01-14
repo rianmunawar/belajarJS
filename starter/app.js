5e42e7561627e559dfa2f3e09659cf42c0bfb919
Rian Munawar <rianmunawar0513@gmail.com>
2022-01-14 10:26:12 +0700

    add function to add todo list and delete them

--- starter/app.js
+++ starter/app.js
@@ -1,3 +1,59 @@
-appTitle = document.querySelector("#app-title");
-
+const appTitle = document.querySelector("#app-title");
 appTitle.textContent = "My ToDo Lists";
+
+// collect UI
+// todoForm
+const todoForm = document.querySelector("#todo-form");
+// todoInput
+const todoInput = document.querySelector("#todo-input");
+// filterInput
+const filterInput = document.querySelector("#filter-input");
+// todoList
+const todoList = document.querySelector("#todo-list");
+// clearButton
+const clearButton = document.querySelector("#clear-todos");
+
+// Add EventListener
+
+todoForm.addEventListener("submit", addTodo);
+todoList.addEventListener("click", deleteTodo);
+
+// function add todo
+function addTodo(e) {
+    e.preventDefault();
+
+    // create li element
+    const li = document.createElement("li");
+    // add class for li
+    li.className =
+        "list-group-item d-flex justify-content-between align-items-center mb-1";
+    // append li child
+    li.appendChild(document.createTextNode(todoInput.value));
+    // create delete button with anchor tag
+    const a = document.createElement("a");
+    // tambah attribute href
+    a.href = "#";
+    // add class
+    a.className = "badge badge-danger delete-todo";
+    // create child from a
+    a.innerHTML = "Delete";
+
+    // menyelipkan a menjadi child dari li
+    li.appendChild(a);
+
+    // menampilkan element ke browser dengan memasukan element ke dalam todoList
+    todoList.appendChild(li);
+}
+
+// function delete todo
+function deleteTodo(e) {
+    e.preventDefault();
+
+    // memastikan target yang di klik
+    if (e.target.classList.contains("delete-todo")) {
+        const parent = e.target.parentElement;
+
+        // remove li atau parent dari a
+        parent.remove();
+    }
+}
