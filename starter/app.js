c0056b06fe7ea928907ed61c7d19526c01623b7e
Rian Munawar <rianmunawar0513@gmail.com>
2022-01-14 21:32:44 +0700

    Add delete todo function, clear all todos, and add function to filter todos

--- starter/app.js
+++ starter/app.js
@@ -17,32 +17,42 @@ const clearButton = document.querySelector("#clear-todos");
 
 todoForm.addEventListener("submit", addTodo);
 todoList.addEventListener("click", deleteTodo);
+clearButton.addEventListener("click", clearTodos);
+filterInput.addEventListener("keyup", filterTodos);
 
 // function add todo
 function addTodo(e) {
     e.preventDefault();
 
-    // create li element
-    const li = document.createElement("li");
-    // add class for li
-    li.className =
-        "list-group-item d-flex justify-content-between align-items-center mb-1";
-    // append li child
-    li.appendChild(document.createTextNode(todoInput.value));
-    // create delete button with anchor tag
-    const a = document.createElement("a");
-    // tambah attribute href
-    a.href = "#";
-    // add class
-    a.className = "badge badge-danger delete-todo";
-    // create child from a
-    a.innerHTML = "Delete";
+    // Handle supaya user tidak bisa menambah todo kosong
+    if (todoInput.value) {
+        // create li element
+        const li = document.createElement("li");
+        // add class for li
+        li.className =
+            "todo-item list-group-item d-flex justify-content-between align-items-center mb-1";
+        // append li child
+        li.appendChild(document.createTextNode(todoInput.value));
+        // create delete button with anchor tag
+        const a = document.createElement("a");
+        // tambah attribute href
+        a.href = "#";
+        // add class
+        a.className = "badge badge-danger delete-todo";
+        // create child from a
+        a.innerHTML = "Delete";
 
-    // menyelipkan a menjadi child dari li
-    li.appendChild(a);
+        // menyelipkan a menjadi child dari li
+        li.appendChild(a);
 
-    // menampilkan element ke browser dengan memasukan element ke dalam todoList
-    todoList.appendChild(li);
+        // menampilkan element ke browser dengan memasukan element ke dalam todoList
+        todoList.appendChild(li);
+
+        // Membersihkan todoInput setelah submit
+        todoInput.value = "";
+    } else {
+        alert("Upps Sorry. Todo tidak boleh kosong !");
+    }
 }
 
 // function delete todo
@@ -51,9 +61,36 @@ function deleteTodo(e) {
 
     // memastikan target yang di klik
     if (e.target.classList.contains("delete-todo")) {
-        const parent = e.target.parentElement;
+        // konfirmasi sebelum menghapus
+        if (confirm("Yakin mau di hapus ?")) {
+            const parent = e.target.parentElement;
 
-        // remove li atau parent dari a
-        parent.remove();
+            // remove li atau parent dari a
+            parent.remove();
+        }
     }
+}
+
+// function clear All todo
+function clearTodos() {
+    if (todoList.childElementCount != 0) {
+        todoList.innerHTML = "";
+    } else {
+        alert("Kamu tidak punya ToDo hari ini");
+    }
+}
+
+// function Filter todos
+function filterTodos(e) {
+    const filterText = e.target.value.toLowerCase();
+    const todoItems = document.querySelectorAll(".todo-item");
+    todoItems.forEach((item) => {
+        const textItem = item.firstChild.textContent.toLowerCase();
+
+        if (textItem.indexOf(filterText) !== -1) {
+            item.setAttribute("style", "display: block;");
+        } else {
+            item.setAttribute("style", "display: none !important;");
+        }
+    });
 }
